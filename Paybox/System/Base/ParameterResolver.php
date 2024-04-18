81780166855deb6e26315c7a48b14223eded3ad0
Kev <symfonyaml@users.noreply.github.com>
2024-04-18 15:02:12 +0200

    Add PBX_SHOPPINGCART and PBX_BILLING parameters

--- Paybox/System/Base/ParameterResolver.php
+++ Paybox/System/Base/ParameterResolver.php
@@ -62,6 +62,8 @@ class ParameterResolver extends AbstractParameterResolver
         'PBX_TYPECARTE',
         'PBX_TYPEPAIEMENT',
         'PBX_ATTENTE',
+        'PBX_SHOPPINGCART',
+        'PBX_BILLING',
     );
 
     /**
